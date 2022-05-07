c750e364dc076229d059fb34708b9be4dd321aed
AndreettaGabriele <andreetta.gabriele99@gmail.com>
2022-05-07 19:46:17 +0200

    GA$ down convertion implementation
    Implementation of the multiplication between the IQ samples and the reference IQ samples created from the estimated doppler frequency and delay

--- DownconverterFilter.m
+++ DownconverterFilter.m
@@ -4,10 +4,43 @@
 %
 classdef DownconverterFilter < handle
     %DownconverterFilter handles...
+    
+
+    properties (SetAccess=public, GetAccess=public)
+        fsampling
+        % Needed for filter
+        lowPassFilter_f
+        isLowPassFilterConfigured
+    end
+
     methods
         function obj = DownconverterFilter()
             %DownconverterFilter constructor
             %   Create DownconverterFilter class
         end
+
+        function obj = configDownConverter(obj,fsampling)
+            obj.fsampling = fsampling;
+        end
+        
+        function time = timebase(obj,Nsamples)
+            time = (0:Nsamples)/obj.fsampling;
+            time = time';
+        end
+
+        function IQRef = signalsCreation(obj,time,fdoppler,delay)
+            IRef = sin(2*pi*fdoppler*(time+delay));
+            QRef = cos(2*pi*fdoppler*(time+delay));
+            IQRef = [IRef , QRef];
+        end
+
+        function obj = downConverter(obj,IQsamples,fdoppler,delay)
+            obj.downconvertedI = IQsamples.*signalsCreation(timebase(length(IQsamples(:,1))),fdoppler,delay);
+        end
+
+        function filteredSamples = filter(downIQ,passBand)
+            
+        end
+        
     end
 end
