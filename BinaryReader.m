e5053c29a34e89938d68ebd8921e0d2582875035
DavideTomasella <DavideTomasella@users.noreply.github.com>
2022-05-08 23:07:06 +0200

    DT$ Review and test BinaryReader

--- BinaryReader.m
+++ BinaryReader.m
@@ -1,6 +1,6 @@
 %
 % First implementation: Mattia Piana
-% Review and Testing:
+% Review and Testing: Davide Tomasella
 %
 classdef BinaryReader < handle
     %BinaryReader handles...
@@ -9,10 +9,10 @@ classdef BinaryReader < handle
     end
    properties (SetAccess=private, GetAccess=public)
        fullfilename
-       nBit_per_sample
+       nByte_per_sample uint32{mustBeGreaterThan(nByte_per_sample,0),mustBeLessThanOrEqual(nByte_per_sample,4)}
        nSamples
-       nSamples_float
        IQsamples
+       IQsamples_float
    end
     
    methods
@@ -28,6 +28,9 @@ classdef BinaryReader < handle
             %struct and validate the input parameters
             %   dirName: string with absolute or relative path where
             %   configuration file and sampled data are stored
+            if nargin < 4
+                nBit_per_sample = 16;
+            end
             if nargin < 5
                 relativePath = true;
             end
@@ -35,54 +38,80 @@ classdef BinaryReader < handle
                 dirName = fullfile(".",dirName);
             end
             obj.fullfilename = fullfile(dirName,fileName);
-            obj.nBit_per_sample = nBit_per_sample;
+            %DT$ approx at closest multiple of 8 -> Matlab limitation!
+            obj.nByte_per_sample = int16(nBit_per_sample/8);
             obj.nSamples = obj.getNSamplesFromFile();
             obj.isReadFileConfigured = true;
         end
-        %Function readFile that reads a window of the binary file spanning from currentSample
-        %to currentSample+nSamples and instantiate the field IQsamples
-        %with the int16 data
+
+        %                        %
+        % ACTION METHODS         %
+        %                        %
+
+        function n = getNSamplesFromFile(obj)
+            %DT$ ftell return the dimension in bytes + "/2"= 2 components
+            %fseek(fid, 0, 'eof');
+            %n = ftell(fid)/obj.nByte_per_sample/2;
+            try
+                s = dir(obj.fullfilename);         
+                n = s.bytes/obj.nByte_per_sample/2;
+            catch
+                disp("Error file not found: aborted");
+             end
+        end
+
         function obj = readFile(obj,currentSample,nSamples)
+            %Function readFile that reads a window of the binary file spanning from currentSample
+            %to currentSample+nSamples and instantiate the field IQsamples
+            %with the int16 data
             if currentSample+nSamples>obj.nSamples
                 currentSample=obj.nSamples-nSamples;
             end
             if currentSample < 0
-                disp("error")
-                return
+                disp("Error in file reading starting point: set to 0")
+                currentSample = 0;
             end
             tmpData = obj.openReadCloseBinaryFile(currentSample,nSamples);
             %obj.IQsamples = obj.formatSamples(tmpData);
             obj.IQsamples=tmpData;
             clear tmpData
         end
+
+        function oSamples_float = get.IQsamples_float(obj)
+            oSamples_float = single(obj.IQsamples);
+        end
+
+        %                        %
+        % PRIVATE METHODS        %
+        %                        %
      
         function tmpData = openReadCloseBinaryFile(obj,currentSample,nSamples)
-            fid = fopen(obj.fullfilename);
-            if(fid~=-1)
-                fseek(fid, (currentSample-1)*2, 'bof');
-                tmpData = fread(fid,[1 nSamples],'int16');
+            try
+                fid = fopen(obj.fullfilename);
+                if(fid~=-1)
+                    fseek(fid, currentSample*obj.nByte_per_sample*2, 'bof');
+                    tmpData = fread(fid,[2 nSamples],obj.getSampleFormat());
+                else 
+                    disp("Matlab cannot open the file: aborted",obj.fullfilename);
+                end
                 fclose(fid);
-            else 
-                disp("Error in opening the file");
+            catch
+                disp("Error in opening or closing the file: aborted");
             end
         end
 
-        function n = getNSamplesFromFile(obj)
-            fid = fopen(obj.fullfilename);
-            if(fid~=-1)
-                fseek(fid, 0, 'eof');
-                n = ftell(fid)/2;
-                fclose(fid);
+        function format = getSampleFormat(obj)
+            if obj.nByte_per_sample == 1
+                format = "int8=>int16";
+            elseif obj.nByte_per_sample == 2
+                format = "int16=>int16";
+            elseif obj.nByte_per_sample == 3
+                format = "bit24=>int32";
+            elseif obj.nByte_per_sample == 4
+                format = "int32=>int32";
             else
-               disp("Error in opening the file");
-            end 
+                disp("Errore: impossible value for nByte_per_sample")
+            end
         end
-        %implement getNSamplesFromFile (dimensione... numero bits non è
-        %divisibile... )
-        %check currentSample*2*obj.nBit_per_sample
-        %implement openReadCloseBinaryFile (try catch... verificare file
-        %corretto... ) -> fopen, fseek+read(length)/read(skip,length), close
-        %implement (maybe) formatSamples (remove average...)
-        %method get.nSamples_float: ritorna nSamples convertito in single
    end
 end
