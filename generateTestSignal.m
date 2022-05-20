a077847bf01a9f47eb91adbf1cda4b2f94a080a6
DavideTomasella <DavideTomasella@users.noreply.github.com>
2022-05-21 01:40:58 +0200

    DT$ new signals for tests

--- generateTestSignal.m
+++ generateTestSignal.m
@@ -4,27 +4,27 @@ close all
 %% signal
 %mode: 0 = constant, 1 = linear, 2 = triangular, 3 = cosine, 4 = quadratic 
 clear vars
-PARS(1) = struct("name","T_tracking_1","dstart",15.23,"dend",15.23,"everyChip",true,"mode",0,"envelope",0,"noise",0);
-PARS(2) = struct("name","T_tracking_1a","dstart",15.23,"dend",15.23,"everyChip",true,"mode",0,"envelope",1,"noise",0);
-PARS(3) = struct("name","T_tracking_1b","dstart",15.23,"dend",15.23,"everyChip",true,"mode",0,"envelope",1,"noise",1);
-PARS(4) = struct("name","T_tracking_10a","dstart",15.23,"dend",18.23,"everyChip",true,"mode",1,"envelope",1,"noise",0);
-PARS(5) = struct("name","T_tracking_10b","dstart",15.23,"dend",18.23,"everyChip",true,"mode",1,"envelope",1,"noise",1);
-PARS(6) = struct("name","T_tracking_11a","dstart",15.23,"dend",18.23,"everyChip",true,"mode",2,"envelope",1,"noise",0);
-PARS(7) = struct("name","T_tracking_11b","dstart",15.23,"dend",18.23,"everyChip",true,"mode",2,"envelope",1,"noise",1);
-PARS(8) = struct("name","T_tracking_2","dstart",418.7,"dend",418.7,"everyChip",true,"mode",0,"envelope",0,"noise",0);
-PARS(9) = struct("name","T_tracking_2a","dstart",418.7,"dend",418.7,"everyChip",true,"mode",0,"envelope",1,"noise",0);
-PARS(10) = struct("name","T_tracking_2b","dstart",418.7,"dend",418.7,"everyChip",true,"mode",0,"envelope",1,"noise",1);
-PARS(21) = struct("name","T_tracking_20","dstart",418.7,"dend",448.7,"everyChip",true,"mode",1,"envelope",0,"noise",0);
-PARS(11) = struct("name","T_tracking_20a","dstart",418.7,"dend",448.7,"everyChip",false,"mode",1,"envelope",1,"noise",0);
-PARS(12) = struct("name","T_tracking_20b","dstart",418.7,"dend",448.7,"everyChip",false,"mode",1,"envelope",1,"noise",1);
-PARS(13) = struct("name","T_tracking_21a","dstart",418.7,"dend",328.7,"everyChip",true,"mode",1,"envelope",1,"noise",0);
-PARS(14) = struct("name","T_tracking_21b","dstart",418.7,"dend",328.7,"everyChip",true,"mode",1,"envelope",1,"noise",1);
-PARS(15) = struct("name","T_tracking_22a","dstart",418.7,"dend",448.7,"everyChip",true,"mode",2,"envelope",1,"noise",0);
-PARS(16) = struct("name","T_tracking_22b","dstart",418.7,"dend",448.7,"everyChip",true,"mode",2,"envelope",1,"noise",1);
-PARS(17) = struct("name","T_tracking_23a","dstart",418.7,"dend",448.7,"everyChip",true,"mode",4,"envelope",1,"noise",0);
-PARS(18) = struct("name","T_tracking_23b","dstart",418.7,"dend",448.7,"everyChip",true,"mode",4,"envelope",1,"noise",1);
-PARS(19) = struct("name","T_tracking_24a","dstart",418.7,"dend",448.7,"everyChip",true,"mode",3,"envelope",1,"noise",0);
-PARS(20) = struct("name","T_tracking_24b","dstart",418.7,"dend",448.7,"everyChip",true,"mode",3,"envelope",1,"noise",1);
+PARS(1)  = struct("name","T_tracking_1",  "dstart",15.23,"dend",15.23,"deveryChip",true, "dmode",0,"envelope",0,"inNoise",0);
+PARS(2)  = struct("name","T_tracking_1a", "dstart",15.23,"dend",15.23,"deveryChip",true, "dmode",0,"envelope",1,"inNoise",0);
+PARS(3)  = struct("name","T_tracking_1b", "dstart",15.23,"dend",15.23,"deveryChip",true, "dmode",0,"envelope",1,"inNoise",1);
+PARS(4)  = struct("name","T_tracking_10a","dstart",15.23,"dend",18.23,"deveryChip",true, "dmode",1,"envelope",1,"inNoise",0);
+PARS(5)  = struct("name","T_tracking_10b","dstart",15.23,"dend",18.23,"deveryChip",true, "dmode",1,"envelope",1,"inNoise",1);
+PARS(6)  = struct("name","T_tracking_11a","dstart",15.23,"dend",18.23,"deveryChip",true, "dmode",2,"envelope",1,"inNoise",0);
+PARS(7)  = struct("name","T_tracking_11b","dstart",15.23,"dend",18.23,"deveryChip",true, "dmode",2,"envelope",1,"inNoise",1);
+PARS(8)  = struct("name","T_tracking_2",  "dstart",418.7,"dend",418.7,"deveryChip",true, "dmode",0,"envelope",0,"inNoise",0);
+PARS(9)  = struct("name","T_tracking_2a", "dstart",418.7,"dend",418.7,"deveryChip",true, "dmode",0,"envelope",1,"inNoise",0);
+PARS(10) = struct("name","T_tracking_2b", "dstart",418.7,"dend",418.7,"deveryChip",true, "dmode",0,"envelope",1,"inNoise",1);
+PARS(21) = struct("name","T_tracking_20", "dstart",418.7,"dend",448.7,"deveryChip",true, "dmode",1,"envelope",0,"inNoise",0);
+PARS(11) = struct("name","T_tracking_20a","dstart",418.7,"dend",448.7,"deveryChip",false,"dmode",1,"envelope",1,"inNoise",0);
+PARS(12) = struct("name","T_tracking_20b","dstart",418.7,"dend",448.7,"deveryChip",false,"dmode",1,"envelope",1,"inNoise",1);
+PARS(13) = struct("name","T_tracking_21a","dstart",418.7,"dend",328.7,"deveryChip",true, "dmode",1,"envelope",1,"inNoise",0);
+PARS(14) = struct("name","T_tracking_21b","dstart",418.7,"dend",328.7,"deveryChip",true, "dmode",1,"envelope",1,"inNoise",1);
+PARS(15) = struct("name","T_tracking_22a","dstart",418.7,"dend",448.7,"deveryChip",true, "dmode",2,"envelope",1,"inNoise",0);
+PARS(16) = struct("name","T_tracking_22b","dstart",418.7,"dend",448.7,"deveryChip",true, "dmode",2,"envelope",1,"inNoise",1);
+PARS(17) = struct("name","T_tracking_23a","dstart",418.7,"dend",448.7,"deveryChip",true, "dmode",4,"envelope",1,"inNoise",0);
+PARS(18) = struct("name","T_tracking_23b","dstart",418.7,"dend",448.7,"deveryChip",true, "dmode",4,"envelope",1,"inNoise",1);
+PARS(19) = struct("name","T_tracking_24a","dstart",418.7,"dend",448.7,"deveryChip",true, "dmode",3,"envelope",1,"inNoise",0);
+PARS(20) = struct("name","T_tracking_24b","dstart",418.7,"dend",448.7,"deveryChip",true, "dmode",3,"envelope",1,"inNoise",1);
 
 
 
@@ -50,7 +50,7 @@ date = datestr(now, '_yymmdd_HHMMSS');
 outputFileName = strcat(PARS(p).name,".bin");
  
 
-%% Create symbols
+%% Create bits (message)
 %Corrected pkt
 packet = [0  1  0  1  1  0  0  0  ...
             0  0  0  0  0  0  0  1  ...
@@ -76,17 +76,25 @@ packet = [0  1  0  1  1  0  0  0  ...
 %          0  0  0  0  0  0  0  1  ...
 %          0  0  1  1];
 BITS = repelem(packet,1,inout.settings.nPRN_x_Symbol);
-SYMBOLS = (2 * BITS - 1);
 
-%% Add random symbols in front and tail
-preMLength = 0;
-postMLength = 0;
+%% Add random bits in front and tail
+preBLength = 0;
+postBLength = 0;
+BITS = [int32(rand(1,preBLength)), BITS, int32(rand(1,postBLength))];
+
+%% Create symbols
+SYMBOLS = (2 * BITS - 1);
 %DT$ add always 1 symbol at the end (TEMP FIX FOR INTERP1 SKIPPING LAST SYMBOL)
-SYMBOLS = [rand(1,preMLength), SYMBOLS, rand(1,postMLength) 1];
+SYMBOLS = [SYMBOLS 1];
 
 %% Multiply per PRN
 PRNsequence = 2 * inout.PRNcode - 1; 
 SEQUENCE = reshape(SYMBOLS.*PRNsequence',1,[]);
+
+%% Add PRN peaces
+prePLength = 0; %max 4092
+postPLength = 0;
+SEQUENCE = [PRNsequence(1:prePLength) SEQUENCE PRNsequence(1:postPLength)];
 
 %% Creation of base signal (strecthed due to doppler)
  % array 1 campione per chip -> doppler variabile = lineare
@@ -95,8 +103,8 @@ SEQUENCE = reshape(SYMBOLS.*PRNsequence',1,[]);
                % usarlo in exp(1i*2*pi*fdoppler*..)
 %SEQUENCE=[1 0 1 0 1 1 0 0 1 1 1 0];
 
-advanceEveryChip = PARS(p).everyChip;
-mode = PARS(p).mode; 
+advancedeveryChip = PARS(p).deveryChip;
+mode = PARS(p).dmode; 
 %mode=0 : constant doppler
 %mode=1 : linear increase
 %mode=2 : triangolar increase
@@ -106,7 +114,7 @@ dopStart = PARS(p).dstart;
 dopEnd = PARS(p).dend;
 %create valueDoppler from specification
 nDopplerSteps = length(SEQUENCE);
-if ~advanceEveryChip %CHANGE DOPPLER EVERY CHIP
+if ~advancedeveryChip %CHANGE DOPPLER EVERY CHIP
     nDopplerSteps = nDopplerSteps / (length(inout.PRNcode) * inout.settings.nPRN_x_Symbol);
 end
 timeFraction = (0:nDopplerSteps-1) / nDopplerSteps;
@@ -143,7 +151,7 @@ end
 
 %construct the doppler frequencies and chipRate vectors from valueDoppler
 
-if ~advanceEveryChip
+if ~advancedeveryChip
     valueDoppler = repelem(valueDoppler,1,length(inout.PRNcode)*inout.settings.nPRN_x_Symbol);
 end
 newChipRate = inout.settings.chipRate + valueDoppler; %add doppler
@@ -160,26 +168,26 @@ genDOPPLER = interp1(tchip,fdoppler,t,"previous");
 %eyediagram(genSIGNAL(1:40920),int16(1/inout.settings.chipRate*inout.settings.fSampling))
 
 %% Add front and tail samples
-sigmaPP = 0.0;    % noise variance 
+outSigma = 0.0;    % noise variance 
 preSLength = 0; %samples
 postSLength = 0;
-genSIGNAL = [sigmaPP * randn(1,preMLength), genSIGNAL, sigmaPP * randn(1,postMLength)];
+genSIGNAL = [outSigma * randn(1,preBLength), genSIGNAL, outSigma * randn(1,postBLength)];
 
 %% Add noise
-sigma = PARS(p).noise;    % noise variance 
-noise = sigma / sqrt(2) * randn(1, length(t))';
+inSigma = PARS(p).inNoise;    % noise variance 
+noise = inSigma / sqrt(2) * randn(1, length(t))';
 genSIGNAL = genSIGNAL + noise;
 
 %% Set signal power
-amplitude = 2 ^ (inout.settings.quantizationBits - 2); % gain of the signal, otherwise the in16 matrix results made of 1,0 and -1
+attenuation = 0;
+amplitude = 2 ^ (inout.settings.quantizationBits - 2 - attenuation); % gain of the signal, otherwise the in16 matrix results made of 1,0 and -1
 genSIGNAL = amplitude * genSIGNAL;
 
 %% Add doopler envelope and orthogonal noise
-delay = 0;
-phase = 0;   % test for a phase
+phase = 0;
 env = PARS(p).envelope; %If 1 add envelope, 0 just noise
-genSIGNAL = genSIGNAL.*exp(env*1i*2*pi*genDOPPLER.*(t-delay)+phase);
-orthoNOISE = 1i*sigma / sqrt(2) * randn(1, length(t))'.*exp(env*1i*2*pi*genDOPPLER.*(t-delay));
+genSIGNAL = genSIGNAL.*exp(env*1i*(2*pi*genDOPPLER.*t+phase));
+orthoNOISE = 1i*inSigma / sqrt(2) * randn(1, length(t))'.*exp(env*1i*(2*pi*genDOPPLER.*t+phase));
 genSIGNAL = genSIGNAL + orthoNOISE;
 reader.IQsamples_float = [real(genSIGNAL) imag(genSIGNAL)];
 
