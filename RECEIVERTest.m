73b597413f4da0d7b9fcc7106e2c25c25c84e433
DavideTomasella <DavideTomasella@users.noreply.github.com>
2022-06-05 08:56:00 +0200

    DT$ p2

--- RECEIVERTest.m
+++ RECEIVERTest.m
@@ -3,9 +3,9 @@ classdef RECEIVERTest < matlab.unittest.TestCase
     properties (TestParameter)
         addDopplerID = {1,2,3,4,5,6,7,8,9,10} %,11,12,13,14,15
         powerReduceID = {10,9,8,7,6,5,4,3,2,1} %,11,12
-        filterBandMultiplier = {1} %, 0 ,3
+        filterBandMultiplier = {1, 0} % ,3
         % [1e2, 5e2, 1e3, 2e3, 5e3] / 100 = [1, 5, 10, 20, 50]
-        reducedMaxDoppler = {1e2, 5e2, 1e3} %, 1e3, 1e2
+        reducedMaxDoppler = {1e3} %,1e2, 5e2, 1e3, 1e2
         ppSegmentSize = {1, 2, 5, 10}
         nCoherentFractions = {1, 3, 5}
     end
